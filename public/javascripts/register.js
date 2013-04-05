00a9657404df9e068def9fd6503f8e5b33858975
Donovan Burton <gacnt@shaw.ca>
2013-04-05 16:16:56 -0600

    Added validation to Registration form as well as Ajax

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,5 +1,144 @@
 $(function() {
+	$('#successreg').hide();
 	$("#register").submit(function(e) { e.preventDefault();
-		alert('Clicked!');
+		var dataJax = $('#register').serialize();
+		$.ajax({
+			type: 'POST',
+			url: '/register.html',
+			data: dataJax,
+			dataType: "JSON",
+			success: function(data) {
+				$('.regbody').hide();
+				$('#successreg').show();
+				if(data.error) {
+					$('#alertsuccessreg').text(data.error);
+				} else {
+					$('#alertsuccessreg').text(data.success);
+				}
+			}
+		});
 	});
-});
+
+	$('#gobackreg').click(function(e) { e.preventDefault();
+		$('.regbody').show();
+		$('#successreg').hide();
+	});
+
+	$('#alert_user_username').hide();
+    $('#alert_user_email').hide();
+    $('#alert_user_password').hide();
+    $('#alert_user_passwordc').hide();
+    var submit_btn = document.getElementById('registeruser');
+    var username  = $('#user_username');
+    var email     = $('#user_email');
+    var password  = $('#user_password');
+    var passwordc = $('#user_passwordc');
+    submit_btn.disabled = true;
+    $('#user_username').on('keyup change', function() {
+      if (username.val().length <= 0) {
+        $('#alert_user_username').text('First name cannot be blank');
+        submit_btn.disabled = true;
+        $('#alert_user_username').show();
+      }
+      else {
+        $('#alert_user_username').text('');
+        submit_btn.disabled = false;
+        $('#alert_user_username').hide();
+      }
+	    if(username.val() == '') {
+		submit_btn.disabled = true;
+	    }
+	    if(email.val() == '') {
+	    	submit_btn.disabled = true;
+	    }
+	    if(password.val() == '') {
+	    	submit_btn.disabled = true;
+	    }
+	    if(passwordc.val() == '') {
+	    	submit_btn.disabled = true;
+	    }
+   });
+
+    $('#user_email').on('keyup change', function() {
+      if ((email.val().length <= 6) || (!document.getElementById('user_email').checkValidity())) {
+        $('#alert_user_email').text('Email cannot be blank or an invalid format');
+        submit_btn.disabled = true;
+        $('#alert_user_email').show();
+      } else {
+        $('#alert_user_email').text('');
+        submit_btn.disabled = false;
+        $('#alert_user_email').hide();
+      }
+	    if(username.val() == '') {
+		submit_btn.disabled = true;
+	    }
+	    if(email.val() == '') {
+	    	submit_btn.disabled = true;
+	    }
+	    if(password.val() == '') {
+	    	submit_btn.disabled = true;
+	    }
+	    if(passwordc.val() == '') {
+	    	submit_btn.disabled = true;
+	    }
+    });   
+
+    $('#user_password').on('keyup change', function() {
+      if (password.val().length <= 7) {
+        $('#alert_user_password').text('Password cannot be less than 8 characters long');
+        submit_btn.disabled = true;
+        $('#alert_user_password').show();
+      } else {
+        $('#alert_user_password').text('');
+        submit_btn.disabled = false;
+        $('#alert_user_password').hide();
+      }
+
+      if (password.val() !== passwordc.val()) {
+        $('#alert_user_passwordc').text('Passwords do not match');
+        submit_btn.disabled = true;
+        $('#alert_user_passwordc').show();
+      } else {
+        $('#alert_user_passwordc').text('');
+        submit_btn.disabled = false;
+        $('#alert_user_passwordc').hide();
+      }
+	    if(username.val() == '') {
+		submit_btn.disabled = true;
+	    }
+	    if(email.val() == '') {
+	    	submit_btn.disabled = true;
+	    }
+	    if(password.val() == '') {
+	    	submit_btn.disabled = true;
+	    }
+	    if(passwordc.val() == '') {
+	    	submit_btn.disabled = true;
+	    }
+
+    });
+
+    $('#user_passwordc').on('keyup change', function() {
+      if (password.val() !== passwordc.val()) {
+        $('#alert_user_passwordc').text('Passwords do not match');
+        submit_btn.disabled = true;
+        $('#alert_user_passwordc').show();
+      } else {
+        $('#alert_user_passwordc').text('');
+        submit_btn.disabled = false;
+        $('#alert_user_passwordc').hide();
+      }
+	    if(username.val() == '') {
+		submit_btn.disabled = true;
+	    }
+	    if(email.val() == '') {
+	    	submit_btn.disabled = true;
+	    }
+	    if(password.val() == '') {
+	    	submit_btn.disabled = true;
+	    }
+	    if(passwordc.val() == '') {
+	    	submit_btn.disabled = true;
+	    }
+    });
+  });
--- routes/register.js
+++ routes/register.js
@@ -5,6 +5,8 @@ exports.show = function(req, res){
 
 // POST Register new user
 exports.new = function(req, res) {
+	var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
 	var db = require('mongojs').connect('localhost/busapp', ['users']);
 	var crypto = require('crypto');
 	db.users.ensureIndex({email:1}, {unique: true});
@@ -21,16 +23,19 @@ exports.new = function(req, res) {
 
 	if (req.body.user.password !== req.body.user.passwordc) {
 		res.send('Passwords do not match');
+	} else if (!re.test(req.body.user.email)) {
+		res.json({error: 'Not a valid email address.'});
 	} else {
 		var digestPassword = crypto.createHash('sha256').update(req.body.user.password).digest("hex");
 		var user = new User(req.body.user.email, req.body.user.username,
 							digestPassword);
+		console.log(user.email + digestPassword + user.username);
  		// Save user to database
 		db.users.save(user, function(err, savedUser) {
 			if (err) {
-				res.send("Somehow you managed to bypass the jQuery validation ---------->" + err);
+				res.json({ error: 'Something went wrong :('})
 			} else {
-				res.json({ success: 'Account registered successfully!'});
+				res.json({ success: 'Account registered successfully :)'});
 			}
 		});
     }
