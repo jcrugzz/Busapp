d3efcb2d3551a34ff5d5b9d2cb6a4c770fc1552e
Donovan Burton <gacnt@shaw.ca>
2013-04-06 00:20:32 -0600

    Fixed app crashing error

--- routes/session.js
+++ routes/session.js
@@ -8,18 +8,20 @@ exports.new = function(req, res) {
 		this.password = password;
 	}
 
-	var user = new User(req.body.user.email, req.body.user.password);
-	console.log(user.email + user.password);
+	var user = new User(req.body.user.email, req.body.user.password);	
 
 	db.users.find({'email':user.email}, function(err, userFound) {
-		
+		console.log(userFound);
 		if (err) {
 			console.log('We have an error :(');
-		} else if (!userFound) {
+		} else if (userFound.length === 0) {
 			console.log('We couldn\'t find the user ' + user.email);
 			return;
-		} else {
+		} else if (userFound) {
 			console.log('We have fixed the error, hooorraayyy!! ' + userFound[0].email + " " + userFound[0].password)
+			
+
+			
 		} 
 	});
 
