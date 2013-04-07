66b579463af44748ca51a46e7b006e24c7f5c6d3
Donovan Burton <gacnt@shaw.ca>
2013-04-06 23:31:56 -0600

    Working on fixing the mismatch rememberTokens

--- routes/session.js
+++ routes/session.js
@@ -11,34 +11,35 @@ exports.new = function(req, res) {
 	var user = new User(req.body.user.email, digestPassword);	
 
 	db.users.find({'email':user.email}, function(errFind, userFound) {
-		var newToken = crypto.createHash('sha1').update(Math.random() * 5997979 + new Date() + 5*123).digest("hex");
+		var newToken = crypto.createHash('sha1').update(Math.random() * 5997979 + new Date() + 5%123).digest("hex");
 		db.users.update({email: userFound[0].email}, { $set: {rememberToken: newToken} }, function(errUpdate) {
 			if (errUpdate) {
 				console.log('Fail safe. ' + errUpdate);
 			}
-		});
 
-		if (errFind) {
+			if (errFind) {
 			console.log('Fail safe. ' + errFind);
-		} else {
-
-			if (userFound.length === 0) {
-				console.log('We could not find the user.');
-			} else if (errFind) {
-				console.log('We have an error: ' + errFind);
-			} else if (digestPassword !== userFound[0].password) {
-				console.log('Incorrect password.');
 			} else {
-				// TO DO: REMOVE THIS LOG AFTER DEBUGGING DONE
-				console.log('We found the user, AND the passwords match.\n' + 
-							'UserFound: ' + userFound[0].email + '\n' +
-							'UserFound Password: ' + userFound[0].password + '\n' +
-							'User Submitted: ' + user.email + '\n' + 
-							'User Submitted Password: ' + user.password);
-				res.cookie('rememberToken', userFound[0].rememberToken);
-				res.render('index', { title: 'Test' });
+
+				if (userFound.length === 0) {
+					console.log('We could not find the user.');
+				} else if (errFind) {
+					console.log('We have an error: ' + errFind);
+				} else if (digestPassword !== userFound[0].password) {
+					console.log('Incorrect password.');
+				} else {
+					// TO DO: REMOVE THIS LOG AFTER DEBUGGING DONE
+					console.log('We found the user, AND the passwords match.\n' + 
+								'UserFound: ' + userFound[0].email + '\n' +
+								'UserFound Password: ' + userFound[0].password + '\n' +
+								'User Submitted: ' + user.email + '\n' + 
+								'User Submitted Password: ' + user.password);
+					res.cookie('rememberToken', userFound[0].rememberToken);
+					res.render('index', { title: 'Test' });
+				}
 			}
-		}
+
+		});
 	});
 
 }
